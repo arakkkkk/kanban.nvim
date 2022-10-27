1083028272c4c6211085c92d42e93bfe7241e608
kouiti arakawa <e1759251@soka-u.jp>
2022-10-27 19:32:44 +0900

    test

--- lua/kanban/fn/tasks/highlight.lua
+++ lua/kanban/fn/tasks/highlight.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 function M.highlight(kanban, task)
-	-- local lines = vim.fn.getbufline(task.buf_nr, 1, "$")
-	-- local hi = vim.api.nvim_buf_add_highlight
-	-- hi(task.buf_nr, 0, "TaskTitle", 0, 0, -1)
-	-- for i = 2, #lines do
-	-- 	if string.match(lines[i], "^" .. kanban.ops.markdown.due_head .. ".*$") then
-	-- 		hi(task.buf_nr, 0, "TaskDue", i - 1, 0, -1)
-	-- 	elseif string.match(lines[i], "^" .. kanban.ops.markdown.tag_head .. ".*$") then
-	-- 		hi(task.buf_nr, 0, "TaskTag", i - 1, 0, -1)
-	-- 	else
-	-- 		vim.api.nvim_command("echohl WarningMsg")
-	-- 		vim.api.nvim_command("echo 'Format error!'")
-	-- 		vim.api.nvim_command("echohl None")
-	-- 		kanban.fn.tasks.close(task)
-	-- 		kanban.fn.tasks.open(kanban, task)
-	-- 		return
-	-- 	end
-	-- end
-	-- vim.api.nvim_win_set_option(task.win_id, 'winhighlight', 'NormalFloat:TaskFloat')
+	local lines = vim.fn.getbufline(task.buf_nr, 1, "$")
+	local hi = vim.api.nvim_buf_add_highlight
+	hi(task.buf_nr, 0, "TaskTitle", 0, 0, -1)
+	for i = 2, #lines do
+		if string.match(lines[i], "^" .. kanban.ops.markdown.due_head .. ".*$") then
+			hi(task.buf_nr, 0, "TaskDue", i - 1, 0, -1)
+		elseif string.match(lines[i], "^" .. kanban.ops.markdown.tag_head .. ".*$") then
+			hi(task.buf_nr, 0, "TaskTag", i - 1, 0, -1)
+		else
+			vim.api.nvim_command("echohl WarningMsg")
+			vim.api.nvim_command("echo 'Format error!'")
+			vim.api.nvim_command("echohl None")
+			kanban.fn.tasks.close(task)
+			kanban.fn.tasks.open(kanban, task)
+			return
+		end
+	end
+	vim.api.nvim_win_set_option(task.win_id, 'winhighlight', 'NormalFloat:TaskFloat')
 end
 return M
