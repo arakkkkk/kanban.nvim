28769974cedc9e63aaa2d289e6f2c978b02bc2a3
kouiti arakawa <e1759251@soka-u.jp>
2022-10-27 20:45:57 +0900

    short change

--- lua/kanban/fn/tasks/close.lua
+++ lua/kanban/fn/tasks/close.lua
@@ -1,5 +1,8 @@
 local M = {}
 function M.close(task)
+	if task.buf_nr == nil then
+		return
+	end
 	pcall(vim.cmd.bdelete, task.buf_nr)
 	task.win_id = nil
 	task.buf_nr = nil
--- lua/kanban/fn/tasks/close_all.lua
+++ lua/kanban/fn/tasks/close_all.lua
@@ -4,7 +4,7 @@ function M.close_all(kanban)
 	for i = 1, #lists do
 		local list = kanban.items.lists[i]
 		for j in pairs(list.tasks) do
-			pcall(vim.cmd.bdelete, list.tasks[j].buf_nr)
+			kanban.fn.tasks.close(list.tasks[j])
 		end
 	end
 end
