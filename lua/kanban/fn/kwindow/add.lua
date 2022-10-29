04672817878c7eac89c6b208a66d94dac9fe7181
kinoshitalab <kouiti@arakkk.local>
2022-10-29 11:39:19 +0900

    test

--- lua/kanban/fn/kwindow/add.lua
+++ lua/kanban/fn/kwindow/add.lua
@@ -21,7 +21,7 @@ function M.add(kanban)
 	hi(kanban.items.kwindow.buf_nr, 0, "ListTitle", 1, 0, -1)
 	local win = vim.api.nvim_open_win(kanban.items.kwindow.buf_nr, true, kanban.items.kwindow.buf_conf)
 	-- kanban.fn.kwindow.autocmd(kanban)
-	vim.api.nvim_win_set_option(0, "winhighlight", "NormalFloat:KanbanFloat")
+	vim.api.nvim_win_set_option(win, "winhighlight", "NormalFloat:KanbanFloat")
 	vim.keymap.set("n", ":q<cr>", "<cmd>KanbanClose<cr>", { silent = true, buffer = kanban.items.kwindow.buf_nr })
 	vim.keymap.set("n", "q<cr>", "<cmd>KanbanClose<cr>", { silent = true, buffer = kanban.items.kwindow.buf_nr })
 end
