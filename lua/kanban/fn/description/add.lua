413e7bef8e5326521469aa18b3696e520b6f00bf
arakkk <bdkouiti5013@gmail.com>
2025-05-21 23:57:38 +0900

    #37

--- lua/kanban/fn/description/add.lua
+++ lua/kanban/fn/description/add.lua
@@ -4,10 +4,6 @@ function M.add(kanban)
 	local focus = kanban.fn.tasks.utils.get_focus(kanban)
 	local focused_list = kanban.items.lists[focus.list_num]
 	local task = focused_list.tasks[focus.task_num]
-	if task.title == "" then
-		vim.api.nvim_err_writeln("Error: It is blank task.")
-		return
-	end
 	kanban.items.description = { task = task }
 
 	-- create kanban panel
@@ -29,7 +25,14 @@ function M.add(kanban)
 	if vim.fn.isdirectory(vim.fn.expand(current_md_dir .. "/" .. kanban.ops.markdown.description_folder)) == 0 then
 		vim.fn.mkdir(vim.fn.expand(current_md_dir .. "/" .. kanban.ops.markdown.description_folder))
 	end
-	local file_path = current_md_dir .. "/" .. kanban.ops.markdown.description_folder .. task.title .. ".md"
+
+	-- arrange task_title/description file name
+	local task_title = task.lines[1]
+	task_title = task_title:gsub(" ", "_")
+	task_title = task_title:gsub("%%", "")
+	task_title = task_title:gsub("#", "")
+
+	local file_path = current_md_dir .. "/" .. kanban.ops.markdown.description_folder .. task_title .. ".md"
 	vim.cmd(":e " .. file_path)
 	local bufnr = vim.api.nvim_win_get_buf(0)
 	vim.bo[bufnr]["bufhidden"] = "delete"
--- lua/kanban/fn/description/set_header.lua
+++ lua/kanban/fn/description/set_header.lua
@@ -1,43 +1,50 @@
 local M = {}
 
 local function write_title(task, insert)
-  vim.fn.setline(1, "# " .. task.title)
+	vim.fn.setline(1, "# " .. task.lines[1])
 end
 local function write_tag(task, insert)
-  local tags = ""
-  for _, tag in pairs(task.tag) do
-    tags = tags .. " " .. tag
-  end
-  vim.fn.setline(2, "tag:" .. tags)
+	local tags = ""
+	for _, line in ipairs(task.lines) do
+		-- gmatch を使って全ての #単語 を抽出
+		for tag in line:gmatch("#%w+") do
+			tags = tags .. tag
+		end
+	end
+	vim.fn.setline(2, "tag:" .. tags)
 end
 local function write_due(task, insert)
-  if #task.due == 0 then
-    return
-  end
-  vim.fn.setline(3, "due: " .. task.due[1])
+	local due = ""
+	for _, line in ipairs(task.lines) do
+		-- gmatch を使って全ての #単語 を抽出
+		for _due in line:gmatch("@[^%s]+") do
+			due = _due
+			break
+		end
+	end
+	vim.fn.setline(3, "due: " .. due)
 end
 local function write_created(task, insert)
-  vim.fn.setline(4, "created: " .. os.date("%Y/%m/%d"))
+	vim.fn.setline(4, "created: " .. os.date("%Y/%m/%d"))
 end
 local function write_updated(task, insert)
-  vim.fn.setline(5, "updated: " .. os.date("%Y/%m/%d"))
+	vim.fn.setline(5, "updated: " .. os.date("%Y/%m/%d"))
 end
 
 function M.set_header(kanban)
-  local description = kanban.items.description
-  local task = description.task
+	local description = kanban.items.description
+	local task = description.task
 
-  local current_buf = vim.api.nvim_get_current_buf()
+	local current_buf = vim.api.nvim_get_current_buf()
 	local doc_lines = vim.fn.getbufline(current_buf, 1, "$")
 
 	if #doc_lines == 1 and doc_lines[1] == "" then
-	  write_title(task, false)
-	  write_tag(task, false)
-	  write_due(task, false)
-	  write_created(task, false)
-	  -- write_updated(task, false)
+		write_title(task, false)
+		write_tag(task, false)
+		write_due(task, false)
+		write_created(task, false)
+		-- write_updated(task, false)
 	end
 end
 
 return M
-
